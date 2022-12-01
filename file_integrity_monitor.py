09ca845bfa168f62845cc3169e7d169d1849f03f
willjroberts <119632066+willjroberts@users.noreply.github.com>
2022-12-01 15:11:00 -0800

    Add files via upload

---
+++ file_integrity_monitor.py
@@ -0,0 +1,42 @@
+from sys import argv
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+#Initialize
+event_handler = LoggingEventHandler()
+observer = Observer()
+
+def file_monitor(watchlist):
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        handlers = [logging.FileHandler('watchlog.txt', 'a')]   
+                        )
+    
+    with open(watchlist[1]) as f:
+        paths = f.readlines()
+
+    watching = []
+    for i in paths:
+        path = i.rstrip()
+        observer.schedule(event_handler, path, recursive=True)
+        watching.append(observer)
+
+    observer.start()
+    print('Monitoring started...')
+
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        print('Monitoring stopped')
+    observer.join()
+
+file_monitor(argv)
+
+
+
